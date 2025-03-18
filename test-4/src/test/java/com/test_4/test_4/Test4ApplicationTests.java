fdfb54e4ce6002795ff7c2e54d90d84e0b150506
sanjit <sanjit@gmail.com>
2025-03-18 13:51:45 +0530

    first commit

---
+++ test-4/src/test/java/com/test_4/test_4/Test4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test_4.test_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
